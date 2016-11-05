634ec951eb07ec7b21cb274182e3973b89be29da
kenjiru <kenjiru.ro@gmail.com>
2016-11-05 15:45:50 +0100

    Added more modules to the vendor chunk.

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,9 @@ var config = {
     entry: {
         app: src_dir + "/App.tsx",
         dropboxAuth: src_dir + "/DropboxAuthPage.tsx",
-        vendor: ["react", "react-dom", "react-router", "redux-thunk", "react-redux", "redux", "lodash", "material-ui",
-            "material-design-icons"]
+        vendor: ["react", "react-dom", "react-router", "redux-thunk", "react-redux", "redux", "lodash", "moment",
+            "material-ui", "material-design-icons", "react-tap-event-plugin", "draft-js", "draft-convert",
+            "draft-js-import-html", "dropbox", "store", "blob-util", "text-encoding", "x2js"]
     },
     output: {
         publicPath: "/",
