be491ae9e6192a120f7e9d63b2846c79ee67cea7
kenjiru <kenjiru.ro@gmail.com>
2017-11-16 23:16:07 +0100

    Better size for the create folder dialog.

--- src/components/create-folder-dialog/CreateFolderDialog.tsx
+++ src/components/create-folder-dialog/CreateFolderDialog.tsx
@@ -1,11 +1,13 @@
 import * as _ from "lodash";
 import * as React from "react";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import { Dialog, DialogContent, DialogActions, Button, TextField } from "material-ui";
 
-import {IStore} from "../../model/store";
-import {createFolder} from "../../model/actions/local";
-import {IDispatchFunction} from "../../utils/ActionUtil";
+import { IStore } from "../../model/store";
+import { createFolder } from "../../model/actions/local";
+import { IDispatchFunction } from "../../utils/ActionUtil";
+
+import "./CreateFolderDialog.less";
 
 class CreateFolderDialog extends React.Component<ICreateFolderDialogProps, ICreateFolderDialogState> {
     private EMPTY_FOLDER: string = "Folder name cannot be empty";
@@ -32,7 +34,11 @@ class CreateFolderDialog extends React.Component<ICreateFolderDialogProps, ICrea
                     open={this.state.isDialogShown}
                     onRequestClose={this.handleCloseDialog}>
                 <DialogContent>
-                    <TextField helperText="Folder name" value={this.state.folderName} label={this.state.errorText}
+                    <TextField className="folder-name"
+                               helperText="Folder name"
+                               error={this.hasError()}
+                               value={this.state.folderName}
+                               label={this.state.errorText}
                                onChange={this.handleInputChange}/>
                 </DialogContent>
                 <DialogActions>
@@ -88,6 +94,10 @@ class CreateFolderDialog extends React.Component<ICreateFolderDialogProps, ICrea
 
     private isEmpty(): boolean {
         return _.isNil(this.state.folderName) || this.state.folderName === "";
+    }
+
+    private hasError(): boolean {
+        return _.isEmpty(this.state.errorText) === false;
     }
 }
 
