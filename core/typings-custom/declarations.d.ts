d6d92be60d53d7662ca9aaf649b5d27f5a67ac5d
kenjiru <kenjiru.ro@gmail.com>
2017-12-08 11:26:04 +0100

    Updated the slate packages.

--- core/typings-custom/declarations.d.ts
+++ core/typings-custom/declarations.d.ts
@@ -3,3 +3,6 @@ declare module "x2js";
 declare module "blob-util";
 declare module "url-parse";
 declare module "slate";
+declare module "slate-react";
+declare module "slate-html-serializer";
+declare module "slate-plain-serializer";
